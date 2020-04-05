66e01530f14e07c14ba019878ef4b68006ae38da
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 23:28:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,78 +1,80 @@
 class TicTacToe
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-    #why pipes
-  end
-  
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [5,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-  
-  def input_to_index(string)
-    index = string.to_i - 1 
-    index
-  end
-    
-  def move(index, token = "X")
-    @board[index] = token
-  end 
-  
-  def position_taken?(index)
-    if @board[index] == "X" or "O"
-      return true 
-    else 
-      return false 
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+def initialize
+  @board = Array.new(9, " ")
+end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index, current_player = "X")
+  @board[index] = current_player
+end
+
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn_count
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
     end
   end
-   
-  def valid_move?(index)
-    if position_taken?(index) == false && [index < 9 || index > -1]
-    then 
-      return true 
-    else 
-      return false
-    end
+  return turn
+end
+
+def current_player
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
   end
-  
-  def turn 
-    puts "Please choose a number 1-9:"
-    move = gets.chomp
-    index = input_to_index(move)
-    if valid_move?(index) == true 
-      token = current_player 
-      move(index, token)
-      display_board
-    else
-      turn
-    end
-  end  
-  
-  #does this work?
-  def turn_count
-    precount = @board.count(" ")
-    count = 9 - precount
-    count
+  return player
+end
+
+def turn
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
+    display_board
+  else
+    turn
   end
-  
-  #does this work?
-  def current_player
-    turns = turn_count
-    if turns % 2 == 0  
-      then "X"
-    else "O"
-    end
-  end
-  
-  def won?
-    # WIN_COMBINATIONS.any? { |n| n==@board }
-    # @board
-    WIN_COMBINATIONS.each {|win_combo|
+end
+
+def won?
+  WIN_COMBINATIONS.each {|win_combo|
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
@@ -88,48 +90,52 @@ class TicTacToe
     end
   }
   return false
+end
+
+def full?
+  @board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?
+  if !won? && full?
+    return true
+  else
+    return false
   end
-  
-  def full?
-    @board.all? { |n| n == "X" || n == "O"}
+end
+
+def over?
+  if won? || draw?
+    return true
+  else
+    return false
   end
-  
-  def draw?
-    if full? == true && won? == false 
-      return true
-    else 
-      return false
-    end 
-  end
-  
-  def over?
-    if won? || draw?
-      return true
-    else 
-      return false
-    end
-  end
-  
-  def winner
-    x = @board.count("X")
-    o = @board.count("O")
-    if x > o 
+end
+
+def winner
+  index = []
+  index = won?
+  if index == false
+    return nil
+  else
+    if @board[index[0]] == "X"
       return "X"
-    else 
+    else
       return "O"
     end
   end
-  
-  def play
-    while over? == false
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}"
-    elsif draw?
-    puts "Cats Game!"
-    end
+end
+
+def play
+  until over? == true
+    turn
   end
-    
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
 end
